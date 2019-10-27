cc10d45cc0f7ff9626fe376bf70c5ac80a8e750e
Robin Preibisius <rpreibisius@gmail.com>
2019-10-26 17:42:38 -0700

    convert f(x) returns a value now

--- dnaMap.js
+++ dnaMap.js
@@ -1,44 +1,43 @@
 function convert(inputStr, strandType) {
-let dnaMap = [];
-
-let bases = strandType == "DNA" ? ['C', 'G', 'A', 'T'] : ['C', 'G', 'A', 'U'];
-for (let i = 32, c = [0, 0, 0, 0, 0]; i <= 127; i++) {
-	let seq="";
-	seq += bases[c[0]];
-  seq += bases[c[1]];
-  seq += bases[c[2]];
-  seq += bases[c[3]];
-  seq += bases[c[4]];
-  
-  dnaMap.push(seq);
-  
-	if (c[4] == 3) {
-  	c[4] = 0;
-    if (c[3] == 3) {
-      c[3] = 0;
-      if (c[2] == 3) {
-        c[2] = 0;
-        if (c[1] == 3) {
-          c[1] = 0;
-          if (c[0] == 3)	c[0] = 0;
-          else c[0]++;
-
-        } else c[1]++;
-      } else c[2]++;
-    } else c[3]++;
-  } else c[4]++;
-}
-
-const dnaString = inputStr.split("").map(
-  (letter, i) => dnaMap[str.charCodeAt(i)-32]).join("");
-
-  module.exports = dnaString;
-}
-
-function complement(inputStrand) {
-
-
-    for (i = 0; i < inputStrand.length; i++) {
-
+    let dnaMap = [];
+    
+    let bases = strandType == "DNA" ? ['C', 'G', 'A', 'T'] : ['C', 'G', 'A', 'U'];
+    for (let i = 32, c = [0, 0, 0, 0, 0]; i <= 127; i++) {
+        let seq="";
+        seq += bases[c[0]];
+      seq += bases[c[1]];
+      seq += bases[c[2]];
+      seq += bases[c[3]];
+      seq += bases[c[4]];
+      
+      dnaMap.push(seq);
+      
+        if (c[4] == 3) {
+          c[4] = 0;
+        if (c[3] == 3) {
+          c[3] = 0;
+          if (c[2] == 3) {
+            c[2] = 0;
+            if (c[1] == 3) {
+              c[1] = 0;
+              if (c[0] == 3)	c[0] = 0;
+              else c[0]++;
+    
+            } else c[1]++;
+          } else c[2]++;
+        } else c[3]++;
+      } else c[4]++;
     }
-}
+    
+    const dnaString = inputStr.split("").map(
+      (letter, i) => dnaMap[str.charCodeAt(i)-32]).join("");
+    
+      module.exports = dnaString;
+      return dnaString;
+    }
+    
+    function complement(inputStrand) {
+        for (i = 0; i < inputStrand.length; i++) {
+    
+        }
+    }
