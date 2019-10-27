e08ae455dc6f493f4faf31a21174350a4bb63edb
abowler <abowler2@gmail.com>
2019-10-26 17:27:50 -0700

    added tests for RNA

--- dnaMap.test.js
+++ dnaMap.test.js
@@ -1,6 +1,6 @@
 const expectExport = require("expect")
 
-
+// DNA tests
 test('a is [  ]', () => {
     expect('a').toBe('[   ]');
 });
@@ -17,3 +17,27 @@ test('cat is [      ]', () => {
     expect('cat').toBe();
 });
 
+test('Cat is [    ]', () => {
+    expect('Cat').toBe([   ]);
+});
+
+//  RNA tests
+test('a is [  ]', () => {
+    expect('a').toBe('[   ]');
+});
+
+test('A is [    ]', () => {
+    expect('A').toBe([   ]);
+});
+
+test('a a is [      ]', () => {
+    expect('a a').toBe([    ]);
+});
+
+test('cat is [      ]', () => {
+    expect('cat').toBe();
+});
+
+test('Cat is [    ]', () => {
+    expect('Cat').toBe([   ]);
+});
